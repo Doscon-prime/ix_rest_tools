e0ab57935e5e5f0df5af7d5c99f4a30fc2e20871
lorithai <lorithai@gmail.com>
2024-11-04 16:52:04 +0100

    version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ix_rest_tools",
-    version="0.1.0",
+    version="0.1.1",
     description="Python library for interacting with Beijer Electronics iX Web Server API.",
     long_description="Python wrapper for Beijer Electronics iX Web Server API",
     long_description_content_type="text/plain",
